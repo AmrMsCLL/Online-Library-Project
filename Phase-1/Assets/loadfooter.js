18ab217de2162c8f792064bd6fc884d105ed19b9
AmrMsCLL <amrsherieycll@gmail.com>
2024-04-07 21:21:07 +0200

    Implemented Reuseable Footer

---
+++ Phase-1/Assets/loadfooter.js
@@ -0,0 +1,8 @@
+fetch('Assets/Footer.html')
+  .then(response => response.text())
+  .then(html => {
+    document.getElementById('footer_placeholder').innerHTML = html;
+  })
+  .catch(error => {
+    console.warn('Error loading the header:', error);
+  });
