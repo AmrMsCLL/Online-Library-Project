b33f8407f8fa12bed2923fdf7a0b9f6b31eb6904
Ahmed Saber <146995775+AHmEeD-SaBER@users.noreply.github.com>
2024-04-13 17:47:20 +0200

    Home Page Has been finished

---
+++ Phase-1/scripts/swipper.js
@@ -0,0 +1,26 @@
+const sliderContents = document.querySelectorAll(".Slider_Content");
+
+sliderContents.forEach(sliderContent => {
+    const sliderButtons = sliderContent.querySelectorAll(".slider_Button");
+    const BooksList = sliderContent.querySelector(".slider");
+
+    const maxScrollLeft = BooksList.scrollWidth - BooksList.clientWidth;
+
+    sliderButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const direction = button.id === "prev" ? -1 : 1;
+            const scrollAmount = BooksList.clientWidth * direction;
+            BooksList.scrollBy({ left: scrollAmount, behavior: "smooth" });
+        });
+    });
+
+    const handleSliderButtons = () => {
+        const limit = 10;
+        sliderButtons[0].style.display = BooksList.scrollLeft <= 0 ? "none" : "block";
+        sliderButtons[1].style.display = BooksList.scrollLeft >= maxScrollLeft-limit ? "none" : "block";
+    };
+
+    BooksList.addEventListener("scroll", handleSliderButtons);
+});
+
+window.addEventListener("load", initiatSwipper);
